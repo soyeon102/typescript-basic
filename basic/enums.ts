9d8e14594a72499665416fd106eb905433aa96f1
soyeon102 <csea9000@naver.com>
2022-11-03 00:23:12 +0900

    백그라운드에서의 enum

--- basic/enums.ts
+++ basic/enums.ts
@@ -21,3 +21,82 @@ enum ArrowKeys {
   LEFT = 'left',
   RIGHT = 'right',
 }
+
+/* 
+  const는 변수로 지정한다는 뜻이 아닌 
+  자바스크립트로 컴파일할 때 직접 지정된 값을 넣어주는 것
+*/
+const enum AnotherOrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+
+/* 
+------------------ TS enum 타입 ------------------
+  enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED,
+  }
+
+  const order = {
+    orderNumber: 3132341,
+    status: OrderStatus.PENDING
+  }  
+
+  const order2 = {
+      orderNumber: 4534232,
+      status: OrderStatus.DELIVERED
+  }
+  ------------------ JS 컴파일 ------------------
+  "use strict";
+  var OrderStatus;
+  (function (OrderStatus) {
+      OrderStatus[OrderStatus["PENDING"] = 0] = "PENDING";
+      OrderStatus[OrderStatus["SHIPPED"] = 1] = "SHIPPED";
+      OrderStatus[OrderStatus["DELIVERED"] = 2] = "DELIVERED";
+      OrderStatus[OrderStatus["RETURNED"] = 3] = "RETURNED";
+  })(OrderStatus || (OrderStatus = {}));
+
+  const order = {
+      orderNumber: 3132341,
+      status: OrderStatus.PENDING
+  };
+
+  const order2 = {
+    orderNumber: 4534232,
+    status: OrderStatus.DELIVERED
+  };
+
+------------------ TS const enum 타입 ------------------
+  const enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED,
+  }
+
+  const order = {
+      orderNumber: 3132341,
+      status: OrderStatus.PENDING
+  }
+  
+  const order2 = {
+      orderNumber: 4534232,
+      status: OrderStatus.DELIVERED
+  }
+------------------ JS 컴파일 ------------------
+  "use strict";
+  const order = {
+    orderNumber: 3132341,
+    status: 0 // OrderStatus.PENDING
+  };
+
+  const order2 = {
+    orderNumber: 4534232,
+    status: 2 // OrderStatus.DELIVERED
+  };
+*/
